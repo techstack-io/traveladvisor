8a92b8a6f8999aa1a8a8621c65fe03de2445c826
techstack-io <81205746+techstack-io@users.noreply.github.com>
2023-01-08 00:28:05 -0600

    Added home page shapes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,17 +15,32 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className='bg-white flex-1 relative'>
+        {/* First Section */}
         <View className='flex-row px-6 mt-16'>
-            <View>
-                <Text className='text-5xl'>
-                    GO
+            <View className='bg-[#1A89FC] items-center justify-center rounded-full p-4'>
+                <Text className='text-5xl text-white p-2'>
+                    Hero
                 </Text>
             </View>
                 <View>
-                    <Text className='text-4xl px-4'>
+                    <Text className='text-5xl text-[#24305E] px-4 my-6 italic'>
                         Travel
                     </Text>
                 </View>
+        </View>
+        {/* Second Section */}
+        <View className='mx-4 px-6 mt-14 space-y-3'>
+            <Text className='text-[#24305E] text-[42px]'>
+                Discover Your
+            </Text>
+            <Text className='text-[#24305E] text-[38px] font-bold'>
+                Paradise
+            </Text>
+        </View>
+        {/* Circle Section */}
+        <View className='w-[400px] h-[400px] bg-[#FCCD04] rounded-full absolute bottom-36 -right-36'>
+        </View>
+        <View className='w-[400px] h-[400px] bg-[#1A89FC] rounded-full absolute -bottom-28 -left-36'>
         </View>
     </SafeAreaView>
   )
